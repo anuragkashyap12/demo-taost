d907a756769f99c3f12ef78e1dca1afc497ab38f
rohitsoni83 <rohit.soni@credgenics.com>
2023-03-20 02:01:15 +0530

    dist file added

---
+++ dist/cjs/types/components/loader.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+export interface LoaderTheme {
+    primary?: string;
+    secondary?: string;
+}
+export declare const LoaderIcon: import("goober").StyledVNode<Omit<import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement> & import("goober").DefaultTheme & LoaderTheme, never>>;
