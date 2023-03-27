4f2f3f807cd8e4b013b5d0d1746df0c9a1519b87
anuragkashyap12 <anurag.kashyap@credgenics.com>
2023-03-27 15:20:28 +0530

    minor change

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: { app: "./src/index.ts", headless: "./src/headless/index.ts" },
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
