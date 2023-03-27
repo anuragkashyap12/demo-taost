e276238d07d79a96902e9ca15900f59014de7001
anuragkashyap12 <anurag.kashyap@credgenics.com>
2023-03-27 13:30:48 +0530

    minor change

--- dist/cjs/types/components/checkmark.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export interface CheckmarkTheme {
-    primary?: string;
-    secondary?: string;
-}
-export declare const CheckmarkIcon: import("goober").StyledVNode<Omit<import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement> & import("goober").DefaultTheme & CheckmarkTheme, never>>;
--- dist/cjs/types/components/loader.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export interface LoaderTheme {
-    primary?: string;
-    secondary?: string;
-}
-export declare const LoaderIcon: import("goober").StyledVNode<Omit<import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement> & import("goober").DefaultTheme & LoaderTheme, never>>;
