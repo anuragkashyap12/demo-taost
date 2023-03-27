58547601ec639e44d90824804e8218b1e2a46b47
anuragkashyap12 <anurag.kashyap@credgenics.com>
2023-03-27 18:15:43 +0530

    minor change

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.[name].js",
     libraryTarget: "umd",
+    library: "cg-toast",
   },
   optimization: {
     minimize: true,
