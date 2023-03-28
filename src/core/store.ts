5733c4308c93ad9b0f01bf49621823bed29747fe
anuragkashyap12 <anurag.kashyap@credgenics.com>
2023-03-28 13:12:43 +0530

    minor fix

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { DefaultToastOptions, Toast, ToastType } from './types';
 
 const TOAST_LIMIT = 20;
